caf63d1f9e78a26e4d539e0fa5bfd9ececa98769
live-beta <wanjala.sam.s@gmail.com>
2016-12-07 23:12:57 +0300

    Update User Functions, Database integration including logins, and user management

--- Classes/users.py
+++ Classes/users.py
@@ -1,72 +1,90 @@
-from db_connect import connection
+import MySQLdb
 
-class user(object):
-    """docstring for users."""
-    def __init__(self,user_name,password,category,db,cur):
-        super(users, self).__init__()
-        self.user_name = user_name
-        self.password = password
-        self.category =category
-        self.db = MySQLdb.connect= (host="localhost",user="root",passwd="andela",db="inventory_management")
-        self.cur=db.cursor()
+from db_connect import dbconnect
 
-class admin(users):
+class admin(object):
     """docstring for admin and operations"""
-    def __init__(self, *arg):
-        self.arg = arg
+    def __init__(self):
+        pass
+
+        #user.__init__(self,user_name,password,category)
+        #self.arg = arg
+        #self.category=category
+        #self.user_name=user_name
+        #self.password=password
+
     def create_admin(self,user_name,password,category):
         con=dbconnect()
         con.connection()
+        cur.db.cursor()
+
         add_stmt = "(INSERT INTO user (name,password,category) VALUES (%s, %s)"
         #add_stmt += add_stmt.format(self.name,self.description,self.cost,self.status, datetime.date(2012, 3, 23))
         data = (self.name,self.password,self.category)
 
-        if cur.execute(add_stmt, data)):
+        if cur.execute(add_stmt, data):
             return True
         else:
             return False
         #code to instancicate variable and post to database
-    def retrieve_user(self,user_name,password):
-        con=dbconnect()
-        con.connection()
-        add_stmt = "(SELECT * FROM user WHERE category=%s"
-        #add_stmt += add_stmt.format(self.name,self.description,self.cost,self.status, datetime.date(2012, 3, 23))
-        data = (self.category)
-
-        if cur.execute(add_stmt, data)):
-            return row[0],row[1],row[2],row[3]
-        else:
-            return False
-        # code to check database and return the users information,this is to demonstrate the addition of user privileges
+    def retrieve_user(self,uname):
+        db = MySQLdb.connect(host="localhost",user="root",passwd="andela",db="inventory_management")
+        cur=db.cursor()
+        #add_stmt = "SELECT * FROM user WHERE password=?",(password)
+        #add_stmt += add_stmt.format(password)
+        #data = ('password':password)
+        #if cur.execute("SELECT * FROM user WHERE password = ?",(password)):
+        #    return row[0],row[1],row[2],row[3]
+        #else:
+        #    return False
+        cur.execute("SELECT * FROM user WHERE name='%s'" %(uname))
+        results=cur.fetchall()
+        print results
+    # code to check database and return the users information,this is to demonstrate the addition of user privileges
     def add_employee(self,user_name,password):
         #Category Set and editable to facilitate promotions
-        con=dbconnect()
-        con.connection()
-        category="employee"
-        add_stmt = "(INSERT INTO user (name,password,category) VALUES (%s, %s)"
+        db = MySQLdb.connect(host="localhost",user="root",passwd="andela",db="inventory_management")
+        cur=db.cursor()
+        category= "employee"
+        cur.execute("INSERT INTO user (name,password,category) VALUES ('%s','%s','%s')" %(user_name,password,category))
+        db.commit()
+        db.close()
+        #category="employee"
+        #add_stmt = "(INSERT INTO user (name,password,category) VALUES (%s, %s)"
         #add_stmt += add_stmt.format(self.name,self.description,self.cost,self.status, datetime.date(2012, 3, 23))
-        data = (self.name,self.password,category)
+        #data = (self.name,self.password,category)
 
-        if cur.execute(add_stmt, data)):
-            return True
-        else:
-            return False
+        #if cur.execute(add_stmt, data):
+        #    return True
+        #else:
+        #    return False
 
-class employees(users):
+class employees(object):
     """docstring for employees."""
-    def __init__(self,user_name,password ):
-        self.user_name = user_name
-        self.password = password
-        self.category='employee'
+    def __init__(self):
+        pass
+        #self.user_name = user_name
+        #self.password = password
+        #self.category='employee'
     def employee_login(self,user_name,password):
         #code to check through tabase to find employee infomation and category
-        con=dbconnect()
-        con.connection()
-        add_stmt = "(SELECT * FROM  user WHERE name=%s AND password=%s"
+        db = MySQLdb.connect(host="localhost",user="root",passwd="andela",db="inventory_management")
+        cur=db.cursor()
+        #con=dbconnect()
+        #con.connection()
+        cur.execute("SELECT * FROM user WHERE name='%s' AND password='%s'" %(user_name,password))
         #add_stmt += add_stmt.format(self.name,self.description,self.cost,self.status, datetime.date(2012, 3, 23))
-        data = (self.name,self.password)
+        result = cur.fetchall()
 
-        if cur.execute(add_stmt, data)):# If a record is found matching the criteria
+        if result:# If a record is found matching the criteria
             return True
         else:
             return False
+
+obj=employees()
+user_name = raw_input("Enter User Name")
+user_password=raw_input("Enter Password")
+
+print "login", obj.employee_login(user_name,user_password)
+
+#print "You Have successfully Added ", obj.retrieve_user(user_name)
